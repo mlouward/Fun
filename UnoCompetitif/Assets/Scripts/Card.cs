be810de209b287d1d48651b594ba52b7c8404757
mlouward <mlouward@gmail.com>
2021-01-05 21:13:10 +0100

    Board and card assets + some logic

---
+++ UnoCompetitif/Assets/Scripts/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public enum CardColors
+{
+    // 0: Red, 1: Yellow, 2: Green, 3: Blue, 4: Wild
+
+    Red,
+    Yellow,
+    Green,
+    Blue,
+    Wild,
+}
+
+public class Card : MonoBehaviour
+{
+    /*
+	* 0-9 are regular
+	* 10 is skip
+	* 11 is reverse
+	* 12 is draw 2
+	* 13 is color change
+	* 14 is +4 color change
+	* 15 is +2 color change
+	* 16 is +1 color change
+	*/
+
+    private CardColors Color { get; }
+    private int Value { get; }
+    private GameObject CardObj;
+
+    public Card(CardColors color, int number, Vector2 position, Quaternion rotation)
+    {
+        string assetName = $"Card_{number}_{(int)color}";  // Example:  "Card_9_1" would be the Yellow 9.
+        GameObject asset = GameObject.Find(assetName);
+        if (asset is null)
+            Debug.LogError("Asset '" + assetName + "' could not be found.");
+        else
+        {
+            CardObj = Instantiate(asset, position, rotation);
+            Color = color;
+            Value = number;
+        }
+    }
+}
---
+++ UnoCompetitif/Assets/Scripts/Deck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    private List<Card> Draw = new List<Card>();
+    private List<Card> Discard = new List<Card>();
+
+    public Deck()
+    {
+        for (int number = 0; number < 10; number++)
+        {
+            for (int color = 0; color < 4; color++)
+            {
+                string assetName = $"Card_{number}_{color}";  // Example:  "Card_9_1" would be the Yellow 9.
+                GameObject card = GameObject.Find(assetName);
+                Draw.Add(new Card((CardColors)color, number, new Vector2(0, 0), new Quaternion(0, 0, 0, 0)));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Shuffles the draw pile (start of game)
+    /// </summary>
+    public void Shuffle()
+    {
+        Draw = Draw.OrderBy(a => Random.value).ToList();
+    }
+
+    /// <summary>
+    /// Used when draw pile is empty to shuffle discard pile.
+    /// </summary>
+    public void MakeDrawFromDiscard()
+    {
+        Draw = Discard.OrderBy(a => Random.value).ToList();
+        Discard = new List<Card>();
+    }
+
+    public Card TakeCard(Player p)
+    {
+        if (Draw.Count == 0)
+            MakeDrawFromDiscard(); // the deck is depleted: reshuffle and continue
+
+        // take the first card off the deck and add it to the discard pile
+        Card card = Draw[0];
+        Draw.RemoveAt(0);
+
+        Discard.Add(card);
+
+        return card;
+    }
+}
