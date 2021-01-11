f97fcf1c7e68e253bceb1f78b04816c879df8297
mlouward <mlouward@gmail.com>
2021-01-11 19:42:30 +0100

    last

--- Problems/Exercism/JavaScript/bob/bob.js
+++ Problems/Exercism/JavaScript/bob/bob.js
@@ -3,6 +3,18 @@
 // convenience to get you started writing code faster.
 //
 
-export const hey = (message) => {
-  throw new Error('Remove this statement and implement this function');
-};
+const isSilence = message => message.trim().length === 0;
+const isShouting = message => /[a-z]/i.test(message) && message === message.toUpperCase();
+const isQuestion = message => message.trim().endsWith('?');
+const isShoutingQuestion = message => isShouting(message) && isQuestion(message);
+const isDefault = () => true;
+
+const rules = [
+  { test: isSilence, response: 'Fine. Be that way!' },
+  { test: isShoutingQuestion, response: 'Calm down, I know what I\'m doing!' },
+  { test: isShouting, response: 'Whoa, chill out!' },
+  { test: isQuestion, response: 'Sure.' },
+  { test: isDefault, response: 'Whatever.' },
+];
+
+export const hey = message => rules.find(({ test }) => test(message)).response;
