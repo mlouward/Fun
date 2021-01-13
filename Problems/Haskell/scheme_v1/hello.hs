b9b1d6848ab438f5bede49377cbfdae5f983f791
mlouward <mlouward@gmail.com>
2021-01-13 18:55:49 +0100

    work

--- Problems/Haskell/scheme_v1/hello.hs
+++ Problems/Haskell/scheme_v1/hello.hs
@@ -1,6 +1,18 @@
 module Main where
+
 import System.Environment
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn("Hello, " ++ args !! 0)
+-- 1.
+-- main = do
+--   args <- getArgs
+--   putStrLn ("Hello, " ++ args !! 0 ++ " and " ++ args !! 1)
+-- 2.
+-- main = do
+--   args <- getArgs
+--   print $ read (head args) + read (args !! 1)
+-- 3.
+main = do
+  putStrLn "Enter some text: "
+  args <- getLine
+  putStrLn args
