0f54c68975ce341b5181f6fc3164cdc3d8456cc9
mlouward <mlouward@gmail.com>
2021-01-14 17:53:15 +0100

    FirstParser

--- Problems/Haskell/scheme_v1/parser.hs
+++ Problems/Haskell/scheme_v1/parser.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           System.Environment (getArgs)
+import           Control.Monad
 import           Text.ParserCombinators.Parsec hiding (spaces)
 
 main :: IO ()
@@ -12,12 +13,36 @@ symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err  -> "No match: " ++ show err
   Right val -> "Found value"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first:rest
+  return
+    $ case atom of
+      "#t" -> Bool True
+      "#f" -> Bool False
+      _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumber
 
 data LispVal = Atom String
              | List [LispVal]
