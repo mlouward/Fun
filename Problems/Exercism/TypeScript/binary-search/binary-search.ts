0f04d0fcef2588a4554afaf22ac020ce7e348725
mlouward <mlouward@gmail.com>
2021-08-26 22:47:13 +0200

    yes

--- Problems/Exercism/TypeScript/binary-search/binary-search.ts
+++ Problems/Exercism/TypeScript/binary-search/binary-search.ts
@@ -2,17 +2,13 @@ export default class BinarySearch {
   array: number[] | undefined;
 
   constructor(arr: number[]) {
-    let tmp = arr[0];
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] < tmp) {
-        this.array = undefined;
-        return;
-      }
-    }
-    this.array = arr;
+    if (arr.every((val, idx, array) => !idx || array[idx - 1] <= val))
+      this.array = arr;
+    else
+      this.array = undefined;
   }
 
-  public indexOf(val: number) {
+  public indexOf(val: number): number {
     if (this.array === undefined) {
       return -1;
     }
