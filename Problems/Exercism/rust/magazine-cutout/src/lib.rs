d5f80bd0333460b550346b1b0a64bdb3940f04de
mlouward <mlouward@gmail.com>
2021-11-19 23:12:05 +0100

    rust

--- Problems/Exercism/rust/magazine-cutout/src/lib.rs
+++ Problems/Exercism/rust/magazine-cutout/src/lib.rs
@@ -5,5 +5,17 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut m: HashMap<&str, i32> = HashMap::new();
+    for elem in magazine {
+        let entry = m.entry(elem).or_insert(0);
+        *entry += 1;
+    }
+    for elem in note {
+        let entry = m.entry(elem).or_insert(0);
+        if *entry == 0 {
+            return false;
+        }
+        *entry -= 1;
+    }
+    true
 }
