2d80283b3b408c220983d9d38bef63a5196e94ed
mlouward <mlouward@gmail.com>
2021-12-29 22:44:33 +0100

    day 7

--- Problems/AdventOfCode2021/day07.py
+++ Problems/AdventOfCode2021/day07.py
@@ -1,3 +1,25 @@
+positions = [int(x) for x in open("./inputs/day7.txt").readline().split(",")]
+from statistics import median, mean
+
 # -----------*** Part 1 ***-----------
 
+best_position = int(median(positions))
+fuel_cost = [abs(x - best_position) for x in positions]
+print(f"Part 1: {sum(fuel_cost)}")
+
 # -----------*** Part 2 ***-----------
+
+
+def sum_of_first_integers(n):
+    return n * (n + 1) // 2
+
+
+max = max(positions) + 1
+# brute-force:
+min_cost = float("inf")
+for pos in range(0, max):
+    tmp = sum([sum_of_first_integers(abs(x - pos)) for x in positions])
+    if tmp < min_cost:
+        min_cost = tmp
+
+print(f"Part 2: {min_cost}")
