60a7fdf27224834e1bb243c7268539b9a80d6fa3
mlouward <mlouward@gmail.com>
2022-01-05 11:32:50 +0100

    day 9 part 1

--- Problems/AdventOfCode2021/day09.py
+++ Problems/AdventOfCode2021/day09.py
@@ -1,3 +1,30 @@
+import numpy as np
+
 # -----------*** Part 1 ***-----------
+map = np.array(
+    [list(map(int, list(line.strip()))) for line in open("./inputs/day9.txt")]
+)
+x, y = map.shape
+
+# go through each value of the map and check the values
+# of its neighbours (horizontal and vertical)
+# if current value is smaller than all the neighbours,
+# add current value to the sum result
+result = 0
+for i in range(x):
+    for j in range(y):
+        neighbours = []
+        if i > 0:
+            neighbours.append(map[i - 1, j])
+        if i < x - 1:
+            neighbours.append(map[i + 1, j])
+        if j > 0:
+            neighbours.append(map[i, j - 1])
+        if j < y - 1:
+            neighbours.append(map[i, j + 1])
+        if all(map[i, j] < n for n in neighbours):
+            result += map[i, j] + 1
+
+print(f"Part 1: {result}")
 
 # -----------*** Part 2 ***-----------
