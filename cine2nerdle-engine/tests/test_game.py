ffa4e6684d8f81be0a018798cedf6c07b463e751
mlouward <mlouward@gmail.com>
2023-12-27 23:45:32 +0100

    addded some tests

--- cine2nerdle-engine/tests/test_game.py
+++ cine2nerdle-engine/tests/test_game.py
@@ -1,7 +1,7 @@
 import pytest
 from neo4j import Driver
 
-from game.play_game import Player
+from game.play_game import Game, Player
 from neo4j_utils.neo4j_utils import GraphDbConnector
 
 
@@ -28,6 +28,37 @@ class TestGame:
                 "Tom Holland",
             ]
 
+    def test_start_game(self):
+        game = Game()
+        game.start_game(1, [1, 2, 3])
+        assert game.current_movie == 1
+        assert game.links_count == {1: 0, 2: 0, 3: 0}
+        assert game.movies_played == [1]
+        assert game.turn == 0
+        assert game.game_over is False
+
+    def test_set_initial_movie(self, db_connector: GraphDbConnector):
+        game = Game()
+        game.set_initial_movie("The Matrix", 1999)
+        movie, links = db_connector.get_id_and_links_from_movie_title_and_year(
+            "The Matrix", 1999
+        )
+        assert game.current_movie == movie
+        assert game.links_count == {link: 0 for link in links}
+        assert game.movies_played == [movie]
+        assert game.turn == 0
+        assert game.game_over is False
+
+    def test_set_initial_movie_nonexistant(self):
+        game = Game()
+        with pytest.raises(ValueError):
+            game.set_initial_movie("zzzzzzzzzzzzzz", 1999)
+
+    def test_set_initial_movie_multiple_movies(self):
+        game = Game()
+        with pytest.warns(UserWarning):
+            game.set_initial_movie("the", 1999)
+
 
 class TestQueries:
     def test_get_id_from_name(self):
@@ -37,7 +68,3 @@ class TestQueries:
         with pytest.warns(UserWarning):
             # expect most popular to be returned (Tom Hanks)
             assert GraphDbConnector.get_id_from_person_name("Tom") == 31
-
-    def test_get_possible_movies(self, db_connector: GraphDbConnector):
-        # movie_id 597 = Titanic
-        assert db_connector.get_possible_movies(597) == [1721, 597]
